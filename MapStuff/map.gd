0a8e16cb66be0fc574c3e18862ae213c5036564a
unknown <jaymertus@gmail.com>
2025-01-13 21:32:36 -0500

    Moved Node dction to map node script

--- MapStuff/map.gd
+++ MapStuff/map.gd
@@ -6,9 +6,8 @@ func _ready() -> void:
 	var num: int = 1 #iterator for name
 	for child: Node in get_children(): #STAGE 1: NAMING NODES
 		if child is Node2D and child is not Camera2D:
-			var M_node_area: Node = child.find_child("Map_Node_Area2D")
-			M_node_area.A_node_clicked.connect(pass_name)
 			child.name = str(num) #name all nodes
+			child.A_node_clicked.connect(pass_name)
 			num+=1
 	#print(get_children())
 	var generated_paths:Array
--- MapStuff/map_node.gd
+++ MapStuff/map_node.gd
@@ -1,21 +1,24 @@
 extends Node2D
-
+signal A_node_clicked(Name: String)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Map_Node_Area2D.set_pickable(true) #sets-up the clickable area for the map nodes
 	if get_parent().name != "root":
 		get_parent().update_label.connect(_update_label)
+	
 	pass # Replace with function body.
 	
 func _update_label()-> void:
 	$Label.text = name
 	pass
-	
+
+# Detects when Node is clicked on by mouse
+func _on_map_node_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("Mouse_left_click"): 
+		print("you have clicked on node " + str($Label.text)) #Prints the name of the node that is clicked on
+		A_node_clicked.emit(name)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_a_node_clicked(Name: String) -> void: 
-	print(Name)
-	
