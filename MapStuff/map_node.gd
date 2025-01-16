2830fb00a69b55d74270373772b4cc92ace4f001
unknown <jaymertus@gmail.com>
2025-01-15 20:54:59 -0500

    Improved UI display & building placement

--- MapStuff/map_node.gd
+++ MapStuff/map_node.gd
@@ -1,5 +1,6 @@
 extends Node2D
 signal A_node_clicked(Name: String)
+@export var On_node: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,8 +17,10 @@ func _update_label()-> void:
 # Detects when Node is clicked on by mouse
 func _on_map_node_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("Mouse_left_click"): 
-		print("you have clicked on node " + str($Label.text)) #Prints the name of the node that is clicked on
+		print("you have clicked on Node " + $Label.text) #Prints the name of the node that is clicked on
+		get_parent().find_child("UI").find_child("Dynamic_Clicked").text = "Node " + $Label.text
 		A_node_clicked.emit(name)
+		print(On_node)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
