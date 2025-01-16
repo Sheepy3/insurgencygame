16a6dffbb182ef2efca4eb8a8fc1abb230fdb725
unknown <jaymertus@gmail.com>
2025-01-15 21:22:43 -0500

    Path name outputing

--- MapStuff/path.gd
+++ MapStuff/path.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Node_Path_Area2D.set_pickable(true)
 	look_at(connection) # point towards connection
 	
 	pass # Replace with function body.
@@ -11,3 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_node_path_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("Mouse_left_click"): 
+		print("you have clicked on Path " + name)
