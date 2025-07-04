684fa0d0fce12d5aeafc373b09149bd6c90a8110
Sheepy3 <sheepy680@gmail.com>
2025-07-04 12:14:56 -0400

    removed distanced squared check and quantized node positions to fix bug with sorting

--- MapStuff/generated_map.gd
+++ MapStuff/generated_map.gd
@@ -5,10 +5,9 @@ var testdot:PackedScene = preload("res://MapStuff/TEST_DOT.tscn")
 
 func _ready() -> void:
 	var sum_points:Array
-	var centers := hex_centers(1, s)  # e.g. N=1 ring, R=10
+	var centers := hex_centers(1, s)  # e.g. N=1 ring, s= center to vertex distance
 	for i in range(centers.size()):
 		var v:Vector2 = centers[i]
-		#print("Hex %d: x=%f, y=%f" % [i, v.x, v.y])
 		var new_hex:Node = testhex.instantiate()
 		new_hex.position = v
 		add_child(new_hex)
@@ -17,28 +16,29 @@ func _ready() -> void:
 		
 	var sum_points_pruned:Array
 	for point:Vector2 in sum_points:
+		var V:Vector2 = Vector2(
+				round(point.x), # converts all x coordinates to integers so they aren't weird anymore
+				round(point.y) # this removes error in the y-axis by resetting the y point to an integer and then re-multiplying by h.
+			)
+		
 		var unique:bool = true
 		for q:Vector2 in sum_points_pruned:
-			print(point.distance_squared_to(q))
-			if point.distance_squared_to(q) < 0.25:
+			if V in sum_points_pruned:
 				unique = false
 		if unique:
-			sum_points_pruned.append(point)
+			sum_points_pruned.append(V)
 	sum_points_pruned.sort_custom(func(a:Vector2, b:Vector2)-> bool:
 		if a.y < b.y:           # clearly above → comes first
 			return true
 		if a.y > b.y:           # clearly below → comes after
 			return false
 		return a.x < b.x        # same row → left-to-right
-)
-	print(sum_points_pruned)
+	)
 	for o in range(sum_points_pruned.size()):
 		var new_dot:Node = testdot.instantiate()
 		new_dot.position = sum_points_pruned[o]
 		new_dot.find_child("Label").text = str(o+1)
 		add_child(new_dot)
-			
-		#	print(points)
 
 #generates virtual hexagons 
 func hex_centers(n: int, s: float) -> Array:
