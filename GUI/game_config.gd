7b5dab3a5a0c5c14478e99bc32537cdcafd7a279
Sheepy3 <sheepy680@gmail.com>
2025-07-13 22:19:12 -0400

    new board files

---
+++ GUI/game_config.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+var map_generator:PackedScene = preload("res://MapStuff/generated_map.tscn")
+var size:int = 1
+func _ready() -> void:
+	show()
+	_start_map_gen() #hardcoded disabling config menu
+ 
+
+
+func _on_button_pressed() -> void:
+	_start_map_gen()
+	
+func _start_map_gen() -> void:
+	var spawn_map_generator:Node = map_generator.instantiate()
+	spawn_map_generator.size = size 
+	get_parent().add_child.call_deferred(spawn_map_generator)
+	hide()
+	
+
+func _on_option_button_item_selected(index: int) -> void:
+	size = index
+	pass # Replace with function body.
