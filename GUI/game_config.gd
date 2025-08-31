0a8dcfba3c5664df8cc56c11a008ef5c6a32bb8b
unknown <jaymertus@gmail.com>
2025-08-30 23:53:19 -0400

    Added basic networking (WebRTC)

--- GUI/game_config.gd
+++ GUI/game_config.gd
@@ -1,7 +1,11 @@
 extends CanvasLayer
 var map_generator:PackedScene = preload("res://MapStuff/generated_map.tscn")
 var size:int = 2
+@onready var client:Node = $Client
+
+
 func _ready() -> void:
+	client.lobby_joined.connect(_lobby_joined)
 	show()
 	#_start_map_gen() #hardcoded disabling config menu
  
@@ -20,3 +24,11 @@ func _start_map_gen() -> void:
 func _on_option_button_item_selected(index: int) -> void:
 	size = index
 	pass # Replace with function body.
+
+
+func _on_join_button_pressed() -> void:
+	client.start(%IP.text, %Room.text, true)
+
+
+func _lobby_joined(lobby:String) -> void:
+	print(lobby)
