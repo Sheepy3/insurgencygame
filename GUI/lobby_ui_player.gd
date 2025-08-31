c59bede829271a6bb0a5fce53a39a9dd1ff9d186
Sheepy3 <sheepy680@gmail.com>
2025-08-31 00:45:34 -0400

    added color function and pull resource function to the lobby ui player scene

--- GUI/lobby_ui_player.gd
+++ GUI/lobby_ui_player.gd
@@ -5,7 +5,13 @@ extends Control
 
 func _ready() -> void:
 	#set player color based on player resource, which should be added to this node before added as a child
+	_update_color()
+
+func _update_player_resource() -> void:
+	#get resource from player resource array in overseer and set player_resource to that resource, then call _update_color
 	pass
+	
+func _update_color() -> void:
 	var colorvector:Vector3 = player_resource.color
 	print(colorvector.x)
 	var color:Color = Color(colorvector.x,colorvector.y,colorvector.z,1)
