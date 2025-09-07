558bf0b614201ada632dc847bfa57b23786e35cf
unknown <jaymertus@gmail.com>
2025-09-06 20:04:55 -0400

    Implemented serialization & deserialization of player resources

--- GUI/game_config.gd
+++ GUI/game_config.gd
@@ -6,6 +6,7 @@ var size:int = 2
 
 func _ready() -> void:
 	client.lobby_joined.connect(_lobby_joined)
+	multiplayer.peer_connected.connect(Add_player_resource)
 	show()
 	#_start_map_gen() #hardcoded disabling config menu
  
@@ -13,6 +14,7 @@ func _ready() -> void:
 
 func _on_button_pressed() -> void:
 	_start_map_gen()
+	Overseer.cycle_players()
 	
 func _start_map_gen() -> void:
 	var spawn_map_generator:Node = map_generator.instantiate()
@@ -28,8 +30,16 @@ func _on_option_button_item_selected(index: int) -> void:
 
 func _on_join_button_pressed() -> void:
 	client.start(%IP.text, %Room.text, true)
-
+	#Add_player_resource(1)
 
 func _lobby_joined(lobby:String) -> void:
-	print(lobby)
-	%Room_Code.text = lobby
+	%Roomcode.text = lobby
+	Add_player_resource(1)
+
+func Add_player_resource(ID:int) -> void:
+	if multiplayer.is_server():
+		var Player_resource:Resource = Player.new()
+		Player_resource.Player_ID = ID
+		Overseer.player_list.append(Player_resource)
+		Overseer.Resources_to_rpc()
+		#Overseer.Player_rpc_info["Player " +str(ID)] = [Overseer.Player_resource.Player_ID,Overseer.Player_resource.Player_name,Overseer.Player_resource.color,Overseer.Player_resource.base_list,Overseer.Player_resource.Weapons,Overseer.Player_resource.Money,Overseer.Player_resource.Man_power,Overseer.Player_resource.Victory_points]
--- Resources/Player.gd
+++ Resources/Player.gd
@@ -1,10 +1,10 @@
 extends Resource
 class_name Player
 @export var Player_ID:int
-@export var Player_name:String
-@export var color:Vector3
+@export var Player_name:String 
+@export var color:Vector3 
 @export var base_list:Array
-@export var Weapons:int
-@export var Money:int
-@export var Man_power:int
-@export var Victory_points:int
+@export var Weapons:int 
+@export var Money:int 
+@export var Man_power:int 
+@export var Victory_points:int 
