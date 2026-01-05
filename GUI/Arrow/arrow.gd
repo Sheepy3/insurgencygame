7f1085d71314adeed9c38b40b78a5f9bc7ae9bee
Sheepy3 <sheepy680@gmail.com>
2026-01-05 12:47:26 -0500

    implemented networked flares

--- GUI/Arrow/arrow.gd
+++ GUI/Arrow/arrow.gd
@@ -1,5 +1,11 @@
 extends Node2D
+var light_color:Vector3
 
+func _ready() -> void:
+	$SubViewport/arrow.passed_light_color = light_color
+	$SubViewport/arrow.update_visual()
+
+	
 func _on_timer_timeout() -> void:
 	queue_free()
 	
