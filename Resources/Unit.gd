593a36d9acedafd06854743d78c8f50318a9b1f6
Sheepy3 <sheepy680@gmail.com>
2026-01-09 19:43:47 -0500

    refactor Unit resource to use playerID instead of name

--- Resources/Unit.gd
+++ Resources/Unit.gd
@@ -2,6 +2,6 @@ extends Resource
 class_name Unit
 @export_enum("Fighter", "Influence") var unit_type: int
 @export var unit_state: bool
-@export var player:String
+@export var player_ID:int
 @export var color:Vector3
 @export var offcolor:bool
